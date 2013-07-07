d068e873c8a2100a4601d95a0f9b7e7bfd8213d3
cuijfboy <cuijfboy@gmail.com>
2013-07-07 23:35:09 +0800

    更改目录结构 添加其他文件

---
+++ toolProject/GPS2GPX/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace GPS2GPX
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_trans = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.open_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.转换ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssLabel_status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_trans
+            // 
+            this.button_trans.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button_trans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_trans.Font = new System.Drawing.Font("微软雅黑", 11F);
+            this.button_trans.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button_trans.Location = new System.Drawing.Point(36, 60);
+            this.button_trans.Name = "button_trans";
+            this.button_trans.Size = new System.Drawing.Size(149, 30);
+            this.button_trans.TabIndex = 0;
+            this.button_trans.Text = "开始转换...";
+            this.button_trans.UseVisualStyleBackColor = false;
+            this.button_trans.Click += new System.EventHandler(this.button_trans_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(220, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open_ToolStripMenuItem,
+            this.转换ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // open_ToolStripMenuItem
+            // 
+            this.open_ToolStripMenuItem.Name = "open_ToolStripMenuItem";
+            this.open_ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.open_ToolStripMenuItem.Text = "打开...";
+            this.open_ToolStripMenuItem.Click += new System.EventHandler(this.open_ToolStripMenuItem_Click);
+            // 
+            // 转换ToolStripMenuItem
+            // 
+            this.转换ToolStripMenuItem.Name = "转换ToolStripMenuItem";
+            this.转换ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.转换ToolStripMenuItem.Text = "转换";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.关于ToolStripMenuItem.Text = "关于...";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLabel_status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 105);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(220, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssLabel_status
+            // 
+            this.tssLabel_status.Name = "tssLabel_status";
+            this.tssLabel_status.Size = new System.Drawing.Size(101, 17);
+            this.tssLabel_status.Text = "拖放文件到这里...";
+            this.tssLabel_status.Visible = false;
+            // 
+            // Form_main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(220, 127);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button_trans);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RAW2GPX";
+            this.TransparencyKey = System.Drawing.Color.Cyan;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_trans;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open_ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 转换ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssLabel_status;
+    }
+}
+
---
+++ toolProject/GPS2GPX/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GPS2GPX
+{
+    public partial class Form_main : Form
+    {
+        private String rawFileName;
+        private String gpxFileName;
+        private XmlDocument doc;
+        private XmlNode root;
+        private XmlElement trkseg;
+
+        private String[] rawMsg;
+        private String lat2;
+        private String lon2;
+        private String time2;
+        //private bool gps_msg_ready;
+
+        public Form_main()
+        {
+            InitializeComponent();
+            rawMsg = new String[12];
+        }
+
+        private bool transform(String fileName)
+        {
+            bool answer = false;
+            StreamReader inStr = null;
+            String textLine = null;
+
+            FileInfo textFile = new FileInfo(fileName);
+            inStr = textFile.OpenText();
+            textLine = inStr.ReadLine();
+            this.newGpxFile();
+            textLine = inStr.ReadLine();
+            while (textLine != null && !(textLine.Trim().Equals("")))
+            {
+                this.analyseRawLine(textLine);
+                this.convertDataFomat(rawMsg[3], rawMsg[5], rawMsg[1], rawMsg[0]);
+                this.addTrkpt(this.lat2, this.lon2, this.time2);
+                textLine = inStr.ReadLine();
+            }
+            inStr.Close();
+            gpxFileName = rawFileName.Substring(0, rawFileName.LastIndexOf('\\')) + rawFileName.Substring(rawFileName.LastIndexOf('\\'), rawFileName.LastIndexOf('.') - rawFileName.LastIndexOf('\\')) + ".gpx";
+            //MessageBox.Show(gpxFileName);
+            this.saveGpxFile(gpxFileName);
+            MessageBox.Show("转换完毕！");
+            return answer;
+        }
+        private void analyseRawLine(String rawLine)
+        {
+            int temp = 0;
+            int tokenPos = rawLine.IndexOf('|');
+            rawLine = rawLine.Substring(tokenPos + 1);
+            tokenPos = rawLine.IndexOf('|');
+            while (tokenPos > 0)
+            {
+                //MessageBox.Show(rawLine.Substring(0, tokenPos));
+                this.rawMsg[temp++] = rawLine.Substring(0, tokenPos);
+                rawLine = rawLine.Substring(tokenPos + 1);
+                tokenPos = rawLine.IndexOf('|');
+            }
+        }
+
+        private void newGpxFile()
+        {
+            doc = new XmlDocument();
+            root = doc.CreateElement("gpx");
+            doc.AppendChild(root);
+
+            XmlElement trk = doc.CreateElement("trk");
+            root.AppendChild(trk);
+
+            XmlElement name = doc.CreateElement("name");
+            name.InnerText = rawFileName.Substring(rawFileName.LastIndexOf('\\') + 1, rawFileName.LastIndexOf('.') - rawFileName.LastIndexOf('\\') - 1);
+            trk.AppendChild(name);
+
+            trkseg = doc.CreateElement("trkseg");
+            trk.AppendChild(trkseg);
+        }
+
+        private void convertDataFomat(String latStr, String lonStr, String timeStr, String dateStr)
+        {
+            double lat = Convert.ToDouble(latStr.Substring(0, 2)) + Convert.ToDouble(latStr.Substring(2)) / 60;
+            this.lat2 = Convert.ToString(lat);
+            double lon = Convert.ToDouble(lonStr.Substring(0, 3)) + Convert.ToDouble(lonStr.Substring(3)) / 60;
+            this.lon2 = Convert.ToString(lon);
+            this.time2 = "20" + dateStr.Substring(4, 2) + "-" + dateStr.Substring(2, 2) + "-" + dateStr.Substring(0, 2) + "T" + timeStr.Substring(0, 2) + ":" + timeStr.Substring(2, 2) + ":" + timeStr.Substring(4, 2) + "Z";
+        }
+
+        private void addTrkpt(String latStr, String lonStr, String timeStr)
+        {
+            XmlElement trkpt = doc.CreateElement("trkpt");
+            trkpt.SetAttribute("lat", latStr);
+            trkpt.SetAttribute("lon", lonStr);
+
+            XmlElement time = doc.CreateElement("time");
+            time.InnerText = timeStr;
+            trkpt.AppendChild(time);
+
+            trkseg.AppendChild(trkpt);
+        }
+
+        private void saveGpxFile(String filename)
+        {
+            XmlWriter xw = XmlWriter.Create(filename);
+            doc.WriteTo(xw);
+            xw.Flush();
+            xw.Close();
+        }
+        private void openFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "RAW数据文件(*.raw)|*.raw|日志文件(*.log)|*.log|文本文件(*.txt)|*.txt|所有文件(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.rawFileName = openFileDialog.FileName;
+                MessageBox.Show(rawFileName);
+            }
+        }
+
+        private void open_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void button_trans_Click(object sender, EventArgs e)
+        {
+            //transform(rawFileName);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "RAW数据文件(*.raw)|*.raw|日志文件(*.log)|*.log|文本文件(*.txt)|*.txt|所有文件(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                this.rawFileName = openFileDialog.FileName;
+                transform(rawFileName);
+            }
+        }
+    }
+}
